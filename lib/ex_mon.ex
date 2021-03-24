5a32ea73bcb9dcabb769bdf9222e2e57c497a893
Paulo Langer <paulo.langer@xing.com>
2021-03-24 16:07:04 +0000

    adding alias for Player method

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,7 @@
 defmodule ExMon do
+  alias ExMon.Player
+
   def create_player(name, move_avg, move_rnd, move_heal) do
-    ExMon.Player.build(name, move_rnd, move_avg, move_heal)
+    Player.build(name, move_rnd, move_avg, move_heal)
   end
 end

71056a46a9f883224e9aa0898b7039af1cab8184
Paulo Langer <paulo.langer@xing.com>
2021-03-24 19:54:10 +0000

    output attack status

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -10,4 +10,12 @@ defmodule ExMon.Game.Status do
   def print_wrong_move_message(move) do
     IO.puts("#{move} is an invalid move")
   end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("The player damaged computer in #{damage}!")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("The computer damaged player in #{damage}!")
+  end
 end

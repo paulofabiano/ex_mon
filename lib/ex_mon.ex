eb1fa0bb8d199cd75931b77614e089bb93d5df09
Paulo Langer <paulo.langer@xing.com>
2021-03-24 15:54:01 +0000

    adding player struct

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,18 +1,2 @@
 defmodule ExMon do
-  @moduledoc """
-  Documentation for `ExMon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMon.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
